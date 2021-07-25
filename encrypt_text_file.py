acf8ffe40fe21778e4cd725e34bb72f36bc3ada2
ff0000xM <ff0000xm@gmail.com>
2021-07-25 22:27:41 +1000

    poop.txt is encrypted with the key file python file will encrypt/decrypt based on input. VERY basic

--- encrypt_text_file.py
+++ encrypt_text_file.py
@@ -1,6 +1,64 @@
-from cryptography import Fernet
+from cryptography.fernet import Fernet
+import time
+run = True
+file =""
+keyname=""
+keyfile=""
 
-def write_key():
+def write_key(key_name):
+    #generates a new key
+    ts = time.time()
     key = Fernet.generate_key()
-    with open("key.key","wb") as key_file:
+    file_name = key_name + ".key"
+    #writes key to current directory as key.key !DO!NOT!LOSE!
+    with open(file_name,"wb") as key_file:
         key_file.write(key)
+
+def read_key(key_file):
+    key_file = key_file + ".key"
+    #loads the key from current directory name 'key.key'
+    return open(key_file, "rb").read()
+
+def encrypt(filename, key):
+    f = Fernet(key)
+
+    with open(filename, "rb") as file:
+        #read all file data
+        file_data = file.read()
+        encrypted_data = f.encrypt(file_data)
+
+    with open(filename,"wb") as file:
+        file.write(encrypted_data)
+
+def decrypt(filename, key):
+    f = Fernet(key)
+
+    with open(filename, "rb") as file:
+        #read all file data
+        encrypted_data = file.read()
+        decrypted_data = f.decrypt(encrypted_data)
+
+    with open(filename,"wb") as file:
+        file.write(decrypted_data)
+
+if __name__ == "__main__":
+
+    run = True
+
+    while run == True:
+        checklist = input('Would you like to Encrypt or Decrypt? (E/D): ').lower()
+        if checklist == 'e':
+            file = input('Enter file name: ')
+            keyname = input('Name your key file !DO!NOT!LOSE!THIS!: ')
+            write_key(keyname)
+            key = read_key(keyname)
+            encrypt(file, key)
+        elif checklist == 'd':
+            file = input('Enter file name: ')
+            key_file = input('Enter Keyfile name, no .key at the end: ')
+            key = read_key(key_file)
+            decrypt(file, key)
+        elif checklist == 'q':
+            run = Fe
+            alse
+        else: print("Please enter E or D or e or d or Q to (q)uit")
